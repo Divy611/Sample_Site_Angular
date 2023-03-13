bb36b7ced3cd019839de7807f423563b1f402b6c
Divy611 <divy.parikh@hotmail.com>
2023-03-13 19:44:58 +0530

    new file:   src/app/aboutus/aboutus.component.css 	new file:   src/app/aboutus/aboutus.component.html 	new file:   src/app/aboutus/aboutus.component.spec.ts 	new file:   src/app/aboutus/aboutus.component.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/contactus/contactus.component.css 	new file:   src/app/contactus/contactus.component.html 	new file:   src/app/contactus/contactus.component.spec.ts 	new file:   src/app/contactus/contactus.component.ts 	modified:   src/index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,AboutusComponent]
 })
 export class AppModule { }
